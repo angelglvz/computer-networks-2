17a1faf2796c51d8787f482122ae06ba4ef508e8
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-04-01 19:20:41 +0200

    Update server_trial_2.py

--- server_trial_2.py
+++ server_trial_2.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
-"usage: <port>"
-"UDP server"
-
+"Usage: %s -P <port>"
 from socket import *
 from pickle import *
 from sys import *
+import struct
 import time
-from socketserver import *
 
+def initialization_handler():
+	if len(argv)!= 3:
+		print(__doc__ % argv[0])
+		exit(1)
+	if argv[1].lower() != "-p":
+		print(__doc__ % argv[0])
+		exit(1)
 
-def handle(sock, msg, client, n):
-	print('New request', n, client)
-	time.sleep(1) # some job
-	sock.sendto(msg.upper(),client)
+def request_handler(sock, msg, client, n):
+	print('New request: ', n, client)
+	op = struct.unpack("!H", msg[0:2])[0]
+	filenameEnd = msg.find(0, 2)
+	filenameLen = filenameEnd - 2
+	filename = struct.unpack("!"+str(filenameLen)+"s", msg[2:filenameEnd])[0].decode()
+	modeEnd = msg.find(0, filenameEnd+1)
+	modeLen = modeEnd - (filenameEnd + 1)
+	mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
+	option_handler(op, filename)
+	time.sleep(1)
 
-if len(argv) != 2:
-	print(__doc__ % argv[0])
-	exit(1)
+def option_handler(option, filename):
+	if option == 1:
+		read_request(filename)
+	elif option == 2:
+		write_request(filename)
+	else:
+		print("There is an error in the request of the client.")
+
+def read_request(filename):
+
+def write_request(filename):
 	
-def main():
-	sock = socket(AF_INET, SOCK_DGRAM)
-	sock.bind(('', int(argv[1])))
-
+initialization_handler()
+port = int(argv[2])
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind(('', port))
 n = 0
+
 while 1:
 	msg, client = sock.recvfrom(1024)
 	n += 1
-	handle(sock, msg, client, n)
-
-if __name__ == '__main__':
-   try:
-       sys.exit(main())
-   except KeyboardInterrupt:
-       pass
+	request_handler(sock, msg, client, n)
+sock.close()	
