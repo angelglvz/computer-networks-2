a3d376e387b70e09de0d04754597cbfd337b69da
Ángel Gamero <35895251+angelglvz@users.noreply.github.com>
2019-04-01 18:29:14 +0200

    Update server_trial_2.py

--- server_trial_2.py
+++ server_trial_2.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
+"usage: <port>"
+"UDP server"
+
 from socket import *
 from pickle import *
 from sys import *
-from struct import *
 import time
+from socketserver import *
+
 
 def handle(sock, msg, client, n):
 	print('New request', n, client)
-	real_msg = unpack('=2h',msg)
-	print('Type: ', real_msg)
-	time.sleep(1)
-	sock.sendto(msg,client)
+	time.sleep(1) # some job
+	sock.sendto(msg.upper(),client)
 
 if len(argv) != 2:
 	print(__doc__ % argv[0])
 	exit(1)
-
-sock = socket(AF_INET, SOCK_DGRAM)
-sock.bind(('', int(argv[1])))
+	
+def main():
+	sock = socket(AF_INET, SOCK_DGRAM)
+	sock.bind(('', int(argv[1])))
 
 n = 0
 while 1:
 	msg, client = sock.recvfrom(1024)
 	n += 1
 	handle(sock, msg, client, n)
+
+if __name__ == '__main__':
+   try:
+       sys.exit(main())
+   except KeyboardInterrupt:
+       pass
