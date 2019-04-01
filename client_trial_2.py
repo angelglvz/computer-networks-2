6c1f1f4b2df601f4feb8a970e971329cd5a11fc0
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-04-01 17:21:05 +0200

    Update client_trial_2.py

--- client_trial_2.py
+++ client_trial_2.py
@@ -1,23 +1,58 @@
-#!/usr/bin/python3.6
-# -*- coding: utf-8 -*-
-"Usage: %s <server> <port>"
+"Usage: %s -S <server> -P <port>"
 from socket import *
 from pickle import *
 from sys import *
 import struct
 
-if len(argv) != 3:
-	print(__doc__ % argv[0])
-	exit(1)
 
+def initialization_handler():
+	if len(argv) != 5:
+		print(__doc__ % argv[0])
+		exit(1)
+	if argv[1].lower() != "-s":
+		print(__doc__ % argv[0])
+		exit(1)
+	if argv[3].lower() != "-p":
+		print(__doc__ % argv[0])
+		exit(1)	
+
+def analyze(command):
+	args = command.split()
+	if args[0].lower() == "read":
+		if len(args) == 1: exit(1)
+		fileName = args[1].encode()
+		mode = b'NETASCII'
+		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(mode))+'sB', 1, fileName, 0,mode, 0)
+		# ends with ''
+		sock.sendto(data, (destination, port))
+	elif args[0].lower() == "write":
+		fileName = args[1].encode()
+		mode = b'NETASCII'
+		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(mode))+'sB', 2, fileName, 0,mode, 0)
+		# ends with ''
+		sock.sendto(data, (destination, port))
+	elif args[0].lower() == "quit":
+		exit(1)
+	else:
+		print("""\
+The operation introduced is not correct. The available operations are:
+	- read <file>
+	- write <file>
+	- quit
+		""")
+
+initialization_handler()
+destination = argv[2]
+port = int(argv[4])
 sock = socket(AF_INET, SOCK_DGRAM)
 
 while 1:
-	data = struct.pack('=2h',0,1)
-	# ends with ''
+	
+	command = input('TFTP@UDP> ')
 
-	sock.sendto(data, (argv[1], int(argv[2])))
+	if not command: break
+	analyze(command)
 	msg, server = sock.recvfrom(1024)
-	print("Reply is '%s'" % msg.decode())
+	print(struct.unpack('!6s', msg))
 
 sock.close()
