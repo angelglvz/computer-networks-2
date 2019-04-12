8f65eb8c7115bdf7069d281bea9fab27eeae2962
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-04-12 22:12:57 +0200

    Update client_trial_2.py

--- client_trial_2.py
+++ client_trial_2.py
@@ -7,8 +7,21 @@ from sys import *
 import struct
 import math
 import os
+import time
 
-
+modes = {
+	0: "NETASCII"
+} 
+server_error_msg = {
+    0: "Not defined, see error message (if any).",
+    1: "File not found.",
+    2: "Access violation.",
+    3: "Disk full or allocation exceeded.",
+    4: "Illegal TFTP operation.",
+    5: "Unknown transfer ID.",
+    6: "File already exists.",
+    7: "No such user."
+}
 
 def initialization_handler():
 	if len(argv) != 5:
@@ -25,18 +38,21 @@ def order(command):
 	sending = command.split()
 	if sending[0].lower() == "read":
 		if len(sending) == 1: exit(1)
-		fileName = sending[1].encode()
-		mode = b'NETASCII'
-		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(mode))+'sB', 1, fileName, 0,mode, 0)
+		fileName = sending[1]
+		
+		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(modes[0]))+'sB', 1, fileName.encode(), 0,modes[0].encode(), 0)
 		# ends with ''
 		sock.sendto(data, (destination, port))
+		receive_data(fileName)
+
 	elif sending[0].lower() == "write":
 		if len(sending) == 1: exit(1)
-		fileName = sending[1].encode()
-		mode = b'NETASCII'
-		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(mode))+'sB', 2, fileName, 0,mode, 0)
+		fileName = sending[1]
+		
+		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(modes[0]))+'sB', 2, fileName.encode(), 0,modes[0].encode(), 0)
 		# ends with ''
 		sock.sendto(data, (destination, port))
+		send_data(fileName)
 	elif sending[0].lower() == "quit":
 		exit(1)
 	else:
@@ -47,20 +63,60 @@ The operation introduced is not correct. The available operations are:
 	- quit
 		""")
 
-def analyze_msg():
-	arguments = command.split()
-	filename = arguments[1]
+def receive_data(filename):
+	path_name = "/home/raulbs/Descargas/"+filename
+	f=open(path_name,'w')
+	pack = 1
+	try:
+		msg, server = sock.recvfrom(1024)
+	
+	except socket.timeout:
+		data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(modes[0]))+'sB', 1, fileName.encode(), 0,modes[0].encode(), 0)
+		# ends with ''
+		sock.sendto(data, (destination, port))
+		receive_data(filename)
+	else:
+		op = struct.unpack("!H", msg[0:2])[0]
+		
+		if op == 3:
+			pack = struct.unpack("!H", msg[2:4])[0]
+			pack += 1
+			size = len(msg)
+			dataLen = size - 4
+			data = struct.unpack("!"+str(dataLen)+"s", msg[4:size])[0]
+			print(data.decode())
+			f.write(data.decode())
+			send_ack(pack)
+		else:
+			print("There was an error in the receiving packets\n")
+			return
 
 	while 1:
-
-		msg, server = sock.recvfrom(1024)
-
-		op = struct.unpack("!H", msg[0:2])[0]
-
-		if op == 5:
-			error_server(msg)
-			break
-	
+		try:
+			msg, server = sock.recvfrom(1024)
+		
+		except socket.timeout:
+			send_ack(pack)
+			continue
+		else:
+			op = struct.unpack("!H", msg[0:2])[0]
+		
+			if op == 3:
+				pack = struct.unpack("!H", msg[2:4])[0]
+				pack += 1
+				size = len(msg)
+				dataLen = size - 4
+				data = struct.unpack("!"+str(dataLen)+"s", msg[4:size])[0]
+				print(data.decode())
+				f.write(data.decode())
+				send_ack(pack)
+			else:
+				print("There was an error in the receiving packets\n")
+				break
+		
+def send_ack(number):
+	ack_msg = struct.pack('!HH', 4, number)
+	sock.sendto(ack_msg, (destination,port))
 
 def error_server(msg):
 	error_code = struct.unpack("!H", msg[2:4])[0]
@@ -70,19 +126,61 @@ def error_server(msg):
 	print('Error ',error_code,': ',error_string)
 
 def send_data(filename):
-	path_name = "~/Documentos/"+filename
+	path_name = "/home/raulbs/Descargas/"+filename
 	if os.path.isfile(path_name):
 		size = os.path.getsize (path_name) 
-		num_pack = math.ceil(size/1024)
+		num_packs = math.ceil(size/512)
+		print(size)
+		print(num_packs)
+		pack = 0
 		f=open(path_name,'rb')
-		while num_pack:
-			bytestosend = f.read(1024)
-			#data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(mode))+'sB', 1, fileName, 0,mode, 0)
-			sock.sendto(bytestosend, (destination,port))
-			num_pack -= 1
-	else:
-		print("You haven't this file: ",filename)
+		
+		try:
+			msg, server = sock.recvfrom(1024)
 
+		except socket.timeout:
+			data = struct.pack('!H'+str(len(fileName))+'sB'+str(len(modes[0]))+'sB', 2, fileName.encode(), 0,modes[0].encode(), 0)
+			# ends with ''
+			sock.sendto(data, (destination, port))
+			send_data(fileName)
+		else:
+			op = struct.unpack("!H", msg[0:2])[0]
+			if op == 4:
+				pack = struct.unpack("!H", msg[2:4])[0]
+				bytestosend = f.read(512)
+				data = struct.pack('!HH'+str(len(bytestosend))+'s', 3, pack, bytestosend)
+				sock.sendto(data, (destination,port))
+				num_packs -= 1
+			elif op == 5:
+				error_server(msg)
+				return
+			else:
+				print("There was an error in the receiving ACK\n")
+				return
+
+		while num_packs:
+			try:
+				msg, server = sock.recvfrom(1024)
+
+			except socket.timeout:
+				sock.sendto(data, (destination,port))
+				continue
+			else:
+				op = struct.unpack("!H", msg[0:2])[0]
+				if op == 4:
+					pack = struct.unpack("!H", msg[2:4])[0]
+					bytestosend = f.read(512)
+					data = struct.pack('!HH'+str(len(bytestosend))+'s', 3, pack, bytestosend)
+					num_packs -= 1
+					sock.sendto(data, (destination,port))
+				
+				elif op == 5:
+					error_server(msg)
+					break
+				else:
+					print("There was an error in the receiving ACK\n")
+					break
+			
 initialization_handler()
 destination = argv[2]
 port = int(argv[4])
@@ -93,8 +191,9 @@ while 1:
 	command = input('TFTP@UDP> ')
 
 	if not command: break
-	send_data("servidor.py")
-	#order(command)
-	#analyze_msg()
+	
+	order(command)
+	
 
 sock.close()
+
