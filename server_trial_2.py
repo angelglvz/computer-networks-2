4fbf72fd1699a0fb4991ace1f9da4dd9177202d5
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-04-12 22:10:13 +0200

    Update server_trial_2.py

--- server_trial_2.py
+++ server_trial_2.py
@@ -7,6 +7,7 @@ from sys import *
 import struct
 import time
 import os
+import math
 
 server_error_msg = {
     0: "Not defined, see error message (if any).",
@@ -29,26 +30,29 @@ def initialization_handler():
 
 def request_handler(sock, msg, client, n):
 	print('New request: ', n, client)
-	time.sleep(1)
 	op = struct.unpack("!H", msg[0:2])[0]
-	filenameEnd = msg.find(0, 2)
-	filenameLen = filenameEnd - 2
-	filename = struct.unpack("!"+str(filenameLen)+"s", msg[2:filenameEnd])[0].decode()
-	modeEnd = msg.find(0, filenameEnd+1)
-	modeLen = modeEnd - (filenameEnd + 1)
-	mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
-	option_handler(op, filename)
 
-def option_handler(option, filename):
-	if option == 1:
-		read_request(filename)
-	elif option == 2:
+	if op == 1:
+		filenameEnd = msg.find(0, 2)
+		filenameLen = filenameEnd - 2
+		filename = struct.unpack("!"+str(filenameLen)+"s", msg[2:filenameEnd])[0].decode()
+		modeEnd = msg.find(0, filenameEnd+1)
+		modeLen = modeEnd - (filenameEnd + 1)
+		mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
+		read_request(filename, client)
+	elif op == 2:
+		filenameEnd = msg.find(0, 2)
+		filenameLen = filenameEnd - 2
+		filename = struct.unpack("!"+str(filenameLen)+"s", msg[2:filenameEnd])[0].decode()
+		modeEnd = msg.find(0, filenameEnd+1)
+		modeLen = modeEnd - (filenameEnd + 1)
+		mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
 		write_request(filename)
 	else:
 		print("There is an error in the request of the client.")
 
-def send_ack():
-	ack_msg = struct.pack('!HH', 4, 0)
+def send_ack(number):
+	ack_msg = struct.pack('!HH', 4, number)
 	sock.sendto(ack_msg, client)
 
 def send_error(code):
@@ -56,30 +60,88 @@ def send_error(code):
 	error_msg = struct.pack('!HH'+str(len(error_string))+'sB', 5, code, error_string, 0)
 	sock.sendto(error_msg, client)
 
-def read_request(filename):
+def read_request(filename, client):
 	path_name = "/home/raulbs/Documentos/"+filename
 	if os.path.isfile(path_name):
+		size = os.path.getsize (path_name) 
+		num_packs = math.ceil(size/512)
+		print(size)
+		print(num_packs)
+		pack = 1
 		f=open(path_name,'rb')
-		bytestosend = f.read(100)
-		sock.sendto(bytestosend, client)
+		bytestosend = f.read(512)
+		print(bytestosend.decode())
+		data = struct.pack('!HH'+str(len(bytestosend))+'s', 3, pack, bytestosend)
+	
+
+		while num_packs:
+			try:
+				sock.sendto(data, client)
+				msg, client = sock.recvfrom(1024)
+			except socket.timeout:
+				continue
+			else:
+				op = struct.unpack("!H", msg[0:2])[0]
+				print(op)
+				if op == 4:
+					pack = struct.unpack("!H", msg[2:4])[0]
+					bytestosend = f.read(512)
+					data = struct.pack('!HH'+str(len(bytestosend))+'s', 3, pack, bytestosend)
+					num_packs -= 1
+				elif op == 5:
+					error_server(msg)
+					break
+				else:
+					print("There was an error in the receiving ACK\n")
+					break
+				
 	else:
-		send_error(1)
+		send_error(5)
 
 def write_request(filename):
 	path_name = "/home/raulbs/Documentos/"+filename
 	if os.path.isfile(path_name):
 		send_error(1)
 	else:
-		send_ack()
+		f=open(path_name,'w')
+		pack = 0
+		while 1:
+			try:
+				send_ack(pack)
+				msg, client = sock.recvfrom(1024)
+				
+			except socket.timeout:
+				continue
+			else:
+				op = struct.unpack("!H", msg[0:2])[0]
+			 
+				if op == 3:
+					pack = struct.unpack("!H", msg[2:4])[0]
+					pack += 1
+					size = len(msg)
+					dataLen = size - 4
+					data = struct.unpack("!"+str(dataLen)+"s", msg[4:size])[0]
+					print(data.decode())
+					f.write(data.decode())
+				elif op == 1:
+					read_request(filename)
+					break
+				elif op == 2:
+					write_request(filename)
+					break
+				else:
+					print("There was an error in the receiving packets\n")
+					break
 
 initialization_handler()
 port = int(argv[2])
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.bind(('', port))
+sock.settimeout(20)
 n = 0
 
 while 1:
 	msg, client = sock.recvfrom(1024)
 	n += 1
-	#request_handler(sock, msg, client, n)
+	request_handler(sock, msg, client, n)
 sock.close()	
