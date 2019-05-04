5813cbdfda8e5e83fe575039a628704da96e3c79
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-05-04 22:13:39 +0200

    Update server_TCP.py

--- server_TCP.py
+++ server_TCP.py
@@ -27,17 +27,29 @@ TFTP_OPCODES = {
     'ack': 4,  # ACKNOWLEDGMENT
     'error': 5}  # ERROR
 
+def main():
+	initialization_handler()
+	port = int(argv[2])
+	sock=socket(AF_INET,SOCK_STREAM)
+	sock.bind(('',port))
+	sock.listen(5)
+	n = 0
 
-def initialization_handler():
-	if len(argv)!= 3:
+	while 1:
+		child_sock, client = sock.accept()
+		n += 1
+		_thread.start_new_thread(handle, (child_sock, client, n))
+
+def initialization_handler():					#This method controls that the arguments we pass in the command
+	if len(argv)!= 3:							#line is correct or not.
 		print(__doc__ % argv[0])
 		exit(1)
 	if argv[1].lower() != "-p":
 		print(__doc__ % argv[0])
 		exit(1)
 
-def request_handler(sock, msg, client, n):
-	print('New request: ', n, client)
+def request_handler(sock, msg, client, n):		#This method is used to the handling of the requests sent by the client.
+	print('  New request:', n, "from",client)
 	op = struct.unpack("!H", msg[0:2])[0]
 
 	if op == 1:
@@ -47,7 +59,7 @@ def request_handler(sock, msg, client, n):
 		modeEnd = msg.find(0, filenameEnd+1)
 		modeLen = modeEnd - (filenameEnd + 1)
 		mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
-		read_request(filename, sock)
+		read_request(filename, sock, client)
 	elif op == 2:
 		filenameEnd = msg.find(0, 2)
 		filenameLen = filenameEnd - 2
@@ -55,25 +67,25 @@ def request_handler(sock, msg, client, n):
 		modeEnd = msg.find(0, filenameEnd+1)
 		modeLen = modeEnd - (filenameEnd + 1)
 		mode = struct.unpack("!"+str(modeLen)+"s", msg[filenameEnd+1:modeEnd])[0].decode()
-		write_request(filename, sock)
+		write_request(filename, sock, client)
 	else:
 		print("There is an error in the request of the client.")
 
-def send_data_pack(bytestosend, pack, sock):
+def send_data_pack(bytestosend, pack, sock, client):		#This method is used to send a data pack with the number of this pack and the bytes that suppose to be data.
 	data = struct.pack('!HH'+str(len(bytestosend))+'s', 3, pack, bytestosend)
-	print("Sending to client: PACK ",pack)
+	print("  --> Sending to client "+str(client)+": PACK",pack)
 	sock.send(data)
-def send_error(code, sock):
-	error_string = server_error_msg[code].encode()
-	error_msg = struct.pack('!HH'+str(len(error_string))+'sB', 5, code, error_string, 0)
-	print("Sending to client: ERROR ",code," (",server_error_msg[code],")")
+def send_error(code, sock, client):							#This method is used to send an error with the number of this error and its description.
+	error_string = server_error_msg[code]
+	error_msg = struct.pack('!HH'+str(len(error_string))+'sB', 5, code, error_string.encode(), 0)
+	print("  --> Sending to client "+str(client)+ ": ERROR",code," ("+server_error_msg[code]+")")
 	sock.send(error_msg)
-def send_ack(number, sock):
+def send_ack(number, sock, client):							#This method is used to send an ack pack that is only used who time.
 	ack_msg = struct.pack('!HH', TFTP_OPCODES['ack'], number)
-	print("Sending to client: ACK ",number)
+	print("  --> Sending to client "+str(client)+": ACK",number)
 	sock.send(ack_msg)
 
-def read_request(filename, sock):
+def read_request(filename, sock, client):					#Used to perform a read request
 	path_name = "/home/raulbs/Documentos/"+filename
 	if os.path.isfile(path_name):
 		size = os.path.getsize (path_name) 
@@ -86,19 +98,19 @@ def read_request(filename, sock):
 				break
 			else:
 				bytestosend = f.read(512)
-				send_data_pack(bytestosend, pack, sock)
+				send_data_pack(bytestosend, pack, sock, client)
 				pack += 1
-	else:
-		send_error(1, sock)
+	else:										
+		send_error(1, sock, client)								#If the file doesn't exist in the server we send an error because we can't read this file.
 
-def write_request(filename, sock):
+def write_request(filename, sock, client):						#Used to do a write request
 	path_name = "/home/raulbs/Documentos/"+filename
-	if os.path.isfile(path_name):
-		send_error(6, sock)
+	if os.path.isfile(path_name):						#If the file exists in the server we send an error because we can write in an existing file.
+		send_error(6, sock, client)
 	else:
 		f=open(path_name,'wb')
 		pack = 0
-		send_ack(pack, sock)
+		send_ack(pack, sock, client)							#First we send an ack to tell the client it is possible to send to the server this file.
 		while 1:
 			msg = sock.recv(516)
 
@@ -108,9 +120,9 @@ def write_request(filename, sock):
 			 
 				if op == 3:
 					packReceived = struct.unpack("!H", msg[2:4])[0]
-					print("Receiving from client: PACK ", packReceived)
-					if pack == packReceived:
-						pack += 1
+					print("  <-- Receiving from client "+str(client)+": PACK", packReceived)
+					if pack == packReceived:									#If the number of received pack is equal to the number of sequence of
+						pack += 1												#good packets received then we write the data of this packet in the file.
 						size = len(msg)
 						dataLen = size - 4
 						data = struct.unpack("!"+str(dataLen)+"s", msg[4:size])[0]
@@ -123,7 +135,7 @@ def write_request(filename, sock):
 					print("There was an error in the receiving packets\n")
 					break
 
-def handle(sock, client,n):
+def handle(sock, client,n):									#Method to handle the connection of one client to the server
 	print('Client connected:',n,client)
 	num = 0
 	while 1:
@@ -132,14 +144,5 @@ def handle(sock, client,n):
 		request_handler(sock, msg, client, n)
 	sock.close()
 
-initialization_handler()
-port = int(argv[2])
-sock=socket(AF_INET,SOCK_STREAM)
-sock.bind(('',port))
-sock.listen(5)
-n = 0
-
-while 1:
-	child_sock, client = sock.accept()
-	n += 1
-	_thread.start_new_thread(handle, (child_sock, client, n))
+if __name__== "__main__":
+  main()
